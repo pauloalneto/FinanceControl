8103b04b7fb9c1eedf3c52b007508af6704dcc75
Paulo Machado de Almeida Neto <paulo.almeida@targetsoftware.com.br>
2021-08-12 23:41:40 -0300

    conitnuous implementation navbar

--- FinanceControl.Spa.Ui/src/app/utils/components/navbar/navbar.component.ts
+++ FinanceControl.Spa.Ui/src/app/utils/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, QueryList, ViewChildren } from '@angular/core';
+
 
 @Component({
   selector: 'app-navbar',
@@ -10,11 +11,26 @@ export class NavbarComponent implements OnInit {
   @Input() menu: any;
 
   showFiller: boolean = false;
+  isOpen: boolean = false;
+
+  @ViewChildren('htmlMenu') htmlMenu = new QueryList<ElementRef>();
+
 
   constructor() { }
 
 
   ngOnInit(): void {
+  }
+
+  toggleMenu(id: any) {
+    this.htmlMenu.forEach(item => {
+      if (item.nativeElement.id == 'menu_' + id) {
+        if (item.nativeElement.classList.contains('navbar-collapse-items'))
+          item.nativeElement.classList.remove('navbar-collapse-items');
+        else
+          item.nativeElement.classList.add('navbar-collapse-items');
+      }
+    })
   }
 
 }
